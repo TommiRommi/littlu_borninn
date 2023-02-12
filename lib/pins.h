6e00599b79b6e847b6f67c7cac8378518c3b3861
tomas <93524600+TommiRommi@users.noreply.github.com>
2023-02-12 12:35:32 +0000

    Add files via upload

---
+++ lib/pins.h
@@ -0,0 +1,21 @@
+#ifndef __ARDUINO_UNO_PINS__
+#define __ARDUINO_UNO_PINS__
+
+/* pinar 0 og 1 virka ekki  */
+#define p0 	0
+#define p1 	1
+
+#define p2 	2
+#define p3 	3
+#define p4 	4
+#define p5 	5
+#define p6 	6
+#define p7 	7
+#define p8 	8
+#define p9 	9
+#define p10 10
+#define p11	11
+#define p12 12
+#define p13	13
+
+#endif
