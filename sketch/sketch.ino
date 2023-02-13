070d5c7016f8be663287ceb1e0d95f8f590abe9c
tomas <93524600+TommiRommi@users.noreply.github.com>
2023-02-13 12:26:34 +0000

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -6,11 +6,50 @@
 #include <stdint.h>
 #include <pins.h>
 
+// echo = p2
+// trig = p3
+#define echo_pin p2
+#define trig_pin p3
+
+
+inline void record_motion()
+{
+	while(true)
+	{	
+		digitalWrite(trig_pin, LOW);
+		delayMicroseconds(5);
+		digitalWrite(trig_pin, HIGH);
+		delayMicroseconds(10);
+		digitalWrite(trig_pin, LOW);
+		 
+		pinMode(echo_pin, INPUT);		
+		digitalWrite(p4, LOW);
+
+		if(pulseIn(echo_pin, HIGH) < 2920)
+		{
+			digitalWrite(p4, HIGH);
+			break;
+		}
+	}
+
+}
+
+
 int main(void)
 {
 	reset();
 
-	/* kóði kemur hér */
+	pinMode(trig_pin, OUTPUT);
+	pinMode(echo_pin, INPUT);
+	pinMode(p4, OUTPUT);
+
+	while(true)
+	{
+		record_motion();
+		digitalWrite(p4, HIGH);
+		delay(4000);
+		digitalWrite(p4, LOW);
+	}
 
 	for(;;){}
 	return(0);
