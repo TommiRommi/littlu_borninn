2e48f80d267d77558be824c6842c7c41852b1ab4
tomas <93524600+TommiRommi@users.noreply.github.com>
2023-02-22 15:10:31 +0000

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -5,23 +5,37 @@
 #include <Arduino.h>
 #include <Servo.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <pins.h>
 
-/* echo = p2, trig = p3, led_pin = p4 */
+/* pins */
 #define echo_pin 	p2
-#define trig_pin 	p3
-#define led_pin		p4
-#define servo_pin	p5
-
+#define ena_pin 	p3
+#define trig_pin 	p4
+#define led_pin	p7
+#define servo_pin	p8
+#define in1_pin	p12
+#define in2_pin	p13
 /* timing */
-#define SERVO_SPEED		(uint16_t)300
-#define SERVO_LOOP_COUNT	(uint8_t)10
+#define SERVO_SPEED		500
+#define SERVO_LOOP_COUNT	6
+#define DC_SPIN_AMOUNT		SERVO_LOOP_COUNT >> 1
+/* distance */
+#define SERVO_ANGLE	60
+/* sensor */
+#define DISTANCE	2000
 
-Servo servo_p;
+const uint8_t pin_call_order[] PROGMEM = {
+	4, 7, 3, 12, 13
+};
+
+Servo const servo_p;
 
 
 inline void record_motion()
 {
+	digitalWrite(in2_pin, LOW);
+
 	while(true)
 	{
 		digitalWrite(trig_pin, LOW);
@@ -29,9 +43,9 @@ inline void record_motion()
 		digitalWrite(trig_pin, HIGH);
 		delayMicroseconds(10);
 		digitalWrite(trig_pin, LOW);
-		 		
+
 		digitalWrite(led_pin, LOW);
-		if(pulseIn(echo_pin, HIGH) < 2920)
+		if(pulseIn(echo_pin, HIGH) < DISTANCE)
 		{
 			digitalWrite(led_pin, HIGH);
 			break;
@@ -44,9 +58,16 @@ int main(void)
 {
 	reset();
 
-	pinMode(trig_pin, OUTPUT);
-	pinMode(echo_pin, INPUT);
-	pinMode(led_pin, OUTPUT);
+	/* pinmodes */
+	pinMode(echo_pin, INPUT);	
+
+	for(int8_t x = 0; x < sizeof(pin_call_order); x++)
+	{
+		pinMode(pin_call_order[x], OUTPUT);
+	}
+
+	digitalWrite(in1_pin, LOW); 
+	digitalWrite(in2_pin, LOW); 
 
 	servo_p.attach(servo_pin);
 
@@ -57,7 +78,12 @@ int main(void)
 
 		for(uint8_t x = 0; x < SERVO_LOOP_COUNT; x++)
 		{
-			servo_p.write(180);
+			if(x == DC_SPIN_AMOUNT)
+			{
+				digitalWrite(in2_pin, HIGH);
+			}
+
+			servo_p.write(SERVO_ANGLE);
 			delay(SERVO_SPEED);
 			servo_p.write(0);
 			delay(SERVO_SPEED);
