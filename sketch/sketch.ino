a2079a5fdfc6113d99c15490e232abe509e6fdec
tomas <93524600+TommiRommi@users.noreply.github.com>
2023-02-27 11:53:56 +0000

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -7,8 +7,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <pins.h>
-// #include <DFRobotDFPlayerMini.h>
-// #include <SoftwareSerial.h>
+#include <DFRobotDFPlayerMini.h>
+#include <SoftwareSerial.h>
 
 /* pins */
 #define echo_pin 	p2
@@ -18,12 +18,14 @@
 #define servo_pin	p8
 #define in1_pin	p12
 #define in2_pin	p13
+
 /* timing */
 #define SERVO_SPEED		500
 #define SERVO_LOOP_COUNT	6
 #define DC_SPIN_AMOUNT		SERVO_LOOP_COUNT >> 1
+
 /* distance */
-#define SERVO_ANGLE	60
+#define SERVO_ANGLE	90
 /* sensor */
 #define DISTANCE	2000
 
@@ -35,9 +37,11 @@ auto on 	= HIGH;
 auto off 	= LOW;
 
 
-class skeleton_baby : public Servo 
+SoftwareSerial mySoftwareSerial(10, 11);
+
+class skeleton_baby : public Servo, public DFRobotDFPlayerMini
 {
-private:	
+private:
 	void record_motion();
 public:
 	inline void robot_move();	
@@ -57,7 +61,9 @@ void skeleton_baby::record_motion()
 		digitalWrite(trig_pin, off);
 
 		digitalWrite(led_pin, off);
-		if(pulseIn(echo_pin, on) < DISTANCE)
+
+		uint16_t distance = pulseIn(echo_pin, on);
+		if(distance < DISTANCE && distance != 0)
 		{
 			digitalWrite(led_pin, on);
 			break;
@@ -68,22 +74,31 @@ void skeleton_baby::record_motion()
 
 inline void skeleton_baby::robot_move()
 {
-	/* pinmodes */
-	pinMode(echo_pin, INPUT);	
+	mySoftwareSerial.begin(9600);
 
+	/* all pinmodes */	
+	pinMode(echo_pin, INPUT);	
 	for(uint8_t x = 0; x < sizeof(pin_call_order); ++x)
 	{
 		pinMode(pin_call_order[x], OUTPUT);
 	}
 
+	// prevents the DC motor from spinning
 	digitalWrite(in1_pin, off); 
 	digitalWrite(in2_pin, off); 
 
-	this->attach(servo_pin);
+	Servo::attach(servo_pin);
+
+	while(!DFRobotDFPlayerMini::begin(mySoftwareSerial))
+
+	DFRobotDFPlayerMini::volume(10);
+	DFRobotDFPlayerMini::loop(1);
 
 	while(true)
 	{
+		DFRobotDFPlayerMini::pause();
 		record_motion();
+		DFRobotDFPlayerMini::start();
 		digitalWrite(led_pin, on);
 
 		for(uint8_t x = 0; x < SERVO_LOOP_COUNT; ++x)
@@ -93,9 +108,9 @@ inline void skeleton_baby::robot_move()
 				digitalWrite(in2_pin, on);
 			}
 
-			this->write(SERVO_ANGLE);
+			Servo::write(0);
 			delay(SERVO_SPEED);
-			this->write(0);
+			Servo::write(SERVO_ANGLE);
 			delay(SERVO_SPEED);
 		}
 
